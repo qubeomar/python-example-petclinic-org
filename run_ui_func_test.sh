7d1d70470541449e25c618c0dc04c97383ae868b
qubeomar <omar@qubeship.com>
2017-06-20 09:49:22 -0700

    remote debugs.  change pypath to home/app

--- run_ui_func_test.sh
+++ run_ui_func_test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-export PYTHONPATH=/usr/local/lib/python3.5:/usr/local/lib/python3.5/site-packages
+#export PYTHONPATH=/usr/local/lib/python3.5:/usr/local/lib/python3.5/site-packages
+export PYTHONPATH=/home/app
 
 python -m petclinic.tests.ui.homepage_menu_tests $1 $2
